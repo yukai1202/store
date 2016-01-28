18fb336f2ddd080e0ef11f79a2f9816cb3ba6e2a
fishyk1202@gmail.com <fishyk1202@gmail.com>
2016-01-29 00:55:52 +0800

    implement list ui to show category and product, add backbone.js

--- store/store/App_Start/BundleConfig.cs
+++ store/store/App_Start/BundleConfig.cs
@@ -10,9 +10,10 @@ namespace store
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/mob/app.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。

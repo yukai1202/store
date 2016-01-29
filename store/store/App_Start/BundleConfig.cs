d5f31807ea694fc9793f8477d9950a710a0f67b8
fishyk1202@gmail.com <fishyk1202@gmail.com>
2016-01-29 22:13:07 +0800

    revert to prvious code

--- store/store/App_Start/BundleConfig.cs
+++ store/store/App_Start/BundleConfig.cs
@@ -10,10 +10,8 @@ namespace store
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-            //bundles.Add(new ScriptBundle("~/bundles/require").Include(
-            //            "~/Scripts/mob/lib/require/require.js"));
-            //bundles.Add(new ScriptBundle("~/bundles/app").Include(
-            //            "~/Scripts/mob/app.js"));
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/mob/app.js"));
             //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
             //            "~/Scripts/jquery.validate*"));
 
